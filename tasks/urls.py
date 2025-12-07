c6eca186d2fa7a83d67512c16e4d03b871178d3f
Olubisi Folarin <olubisi.folarin@smartdvm.com>
2025-12-07 21:16:36 +0000

    first commit

---
+++ tasks/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import TaskCreateView
+
+urlpatterns = [
+    path('tasks/', TaskCreateView.as_view(), name='task-create'),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import TaskSerializer
 
 # Create your views here.
+
+class TaskCreateView(APIView):
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
